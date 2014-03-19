cc7c022d9dda0793abef446b30a8555ef6567516
Nat Goodspeed <nat@lindenlab.com>
2014-03-19 12:51:06 -0400

    Fix build_desura.sh for ease of development.
    Use 'set -e' rather than requiring '|| exit 1' after each command; the trouble
    with that is that it's spotty, producing spurious success after failed builds.
    
    Accept, as additional $1 commands, the names of the shell functions defined in
    this script: clean, build_desura, build_cef. The current if/elif logic always
    invokes 'clean' before any build activity; when a clean build includes
    downloading and rebuilding Boost and all other dependent libraries, that
    wastes considerable time. As a consequence, we no longer need recognize
    "clean" explicitly in the elif chain. (But we must still recognize "pack"
    because of its extra argument.)
    
    Add a final else clause to fail if the verb is unrecognized, instead of doing
    nothing and claiming success.
    
    Turn on CMAKE_VERBOSE_MAKEFILE to help diagnose problems with compiler switches.
    
    Use "$@" rather than plain $@ so arguments will be correctly forwarded rather
    than reparsed at each level of indirection.
    
    Use bash rather than plain Bourne sh so we can use 'declare' to test for shell
    functions.

--- build_desura.sh
+++ build_desura.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 echo 'Make sure to run \033[1;31msudo ./install_deps.sh\033[0m before compiling!\n'
 
 PREFIX="../install"
 OFFICIAL_BUILD=OFF
+
+# any error should terminate the script
+set -e
 
 clean() {
 	rm -rf build
@@ -17,8 +20,9 @@ build_desura() {
 		mkdir build
 	fi
 	cd build
-	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_CEF=OFF -DWITH_GTEST=OFF -DOFFICIAL_BUILD=$OFFICIAL_BUILD || exit 1
-	make install $@
+	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_CEF=OFF -DWITH_GTEST=OFF -DOFFICIAL_BUILD=$OFFICIAL_BUILD \
+		-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+	make install "$@"
 	cd ..
 	echo 'Building Desura completed'
 }
@@ -28,8 +32,8 @@ build_cef() {
 		mkdir build_cef
 	fi
 	cd build_cef
-	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_ONLY_CEF=ON || exit 1
-	make install $@
+	cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_ONLY_CEF=ON
+	make install "$@"
 	cd ..
 	echo 'Building CEF completed'
 }
@@ -39,8 +43,8 @@ pack() {
 		mkdir build_package
 	fi
 	cd build_package
-	cmake .. -DPACKAGE_TYPE=$PACKAGE -DINSTALL_DESKTOP_FILE=ON -DCMAKE_INSTALL_PREFIX="/opt/desura" -DDESKTOP_EXE="/opt/desura/desura" -DDESKTOP_ICON="/opt/desura/desura.png" || exit 1
-	make package $@
+	cmake .. -DPACKAGE_TYPE=$PACKAGE -DINSTALL_DESKTOP_FILE=ON -DCMAKE_INSTALL_PREFIX="/opt/desura" -DDESKTOP_EXE="/opt/desura/desura" -DDESKTOP_ICON="/opt/desura/desura.png"
+	make package "$@"
 	if [ $PACKAGE = "DEB" ]; then
 		mv Desura-*.deb ..
 	elif [ $PACKAGE = "RPM" ]; then
@@ -52,35 +56,41 @@ pack() {
 
 if [ -z "$1" ]; then
 	clean
-	build_desura || exit 2
-	build_cef || exit 2
-elif [ "$1" = "clean" ]; then
-	shift
-	clean
+	build_desura "$@"
+	build_cef
 elif [ "$1" = "pack" ]; then
 	shift
 	PACKAGE="$1"
 	shift
 	clean
-	pack || exit 2
+	pack "$@"
 elif [ "$1" = "only_desura" ]; then
 	shift
 	clean
-	build_desura || exit 2
+	build_desura "$@"
 elif [ "$1" = "only_cef" ]; then
 	shift
 	clean
-	build_cef || exit 2
+	build_cef "$@"
 elif [ "$1" = "help" ]; then
 	echo 'This script will allow you to easy compile Desura on Linux.'
 	echo 'Type "./build_desura.sh clean" to remove build files.'
 	echo 'Type "./build_desura.sh" to build Desura client.'
 	echo 'Type "./build_desura.sh pack DEB" to prepare .deb package.'
 	echo 'Type "./build_desura.sh pack RPM" to prepare .rpm package.'
-	echo 'Type "./build_desura.sh only_desura" to compile Desura without CEF(Chormium Embedded Framework).'
+	echo 'Type "./build_desura.sh only_desura" to compile Desura without CEF (Chromium Embedded Framework).'
 	echo 'Type "./build_desura.sh only_cef" to compile CEF without Desura.'
 	echo 'Type "./build_desura.sh help" to display this message.'
-	echo 'Any additonal shell argument will be given to "make" command.\n'
+	echo 'Any additional shell arguments will be given to "make" command.\n'
+elif declare -fF "$1" >/dev/null ; then
+	# declare -fF name tests for existence of shell function --
+	# in other words, is $1 the name of one of our functions above?
+	fn="$1"
+	shift
+	"$fn" "$@"
+else
+	echo "Unrecognized command '$1'"
+	exit 1
 fi
 
 echo 'Run \033[1;31m./install/desura\033[0m to start Desura!'
